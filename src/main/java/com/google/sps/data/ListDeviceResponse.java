39ee068fb7aeda9f63b7317a96c9753c675e720b
George Zheng <georgezheng999@gmail.com>
2020-07-22 20:10:21 +0000

    case change

--- src/main/java/com/google/sps/data/ListDeviceResponse.java
+++ src/main/java/com/google/sps/data/ListDeviceResponse.java
@@ -11,19 +11,19 @@ import java.util.List;
 public final class ListDeviceResponse {
 
   private final String kind;
-  private final List<ChromeOSDevice> chromeosdevices;
+  private final List<ChromeOSDevice> chromeOSdevices;
   private final String nextPageToken;
   private final String etag;
 
-  public ListDeviceResponse(String kind, List<ChromeOSDevice> chromeosdevices, String nextPageToken, String etag) {
+  public ListDeviceResponse(String kind, List<ChromeOSDevice> chromeOSdevices, String nextPageToken, String etag) {
     this.kind = kind;
-    this.chromeosdevices = getDeviceListCopy(chromeosdevices);
+    this.chromeOSdevices = getDeviceListCopy(chromeOSdevices);
     this.nextPageToken = nextPageToken;
     this.etag = etag;
   }
 
   public List<ChromeOSDevice> getDeviceListCopy(List<ChromeOSDevice> original) {
-    final List<ChromeOSDevice> devices = new ArrayList<>(chromeosdevices);
+    final List<ChromeOSDevice> devices = new ArrayList<>(chromeOSdevices);
     for (final ChromeOSDevice device : original) {
       devices.add(device.copy());
     }
@@ -31,6 +31,6 @@ public final class ListDeviceResponse {
   }
 
   public List<ChromeOSDevice> getDevices() {
-    return getDeviceListCopy(chromeosdevices);
+    return getDeviceListCopy(chromeOSdevices);
   }
 }
