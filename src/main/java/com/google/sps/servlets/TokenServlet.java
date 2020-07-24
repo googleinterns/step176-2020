053657ed062d0f61a0fc81a05d27830afa59734e
George Zheng <georgezheng999@gmail.com>
2020-07-24 17:25:44 +0000

    can now get devices

--- src/main/java/com/google/sps/servlets/TokenServlet.java
+++ src/main/java/com/google/sps/servlets/TokenServlet.java
@@ -6,19 +6,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import java.security.GeneralSecurityException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-// import com.google.api.services.admin.directory.Directory;
-// import com.google.api.services.admin.directory.DirectoryScopes;
-// import com.google.api.services.admin.directory.model.User;
-// import com.google.api.services.admin.directory.model.Users;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.HttpUrl;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
@@ -35,6 +40,7 @@ public class TokenServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
   }
+
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -61,6 +67,24 @@ public class TokenServlet extends HttpServlet {
 
     System.out.println(accessToken);
     System.out.println("is the access token!");
+
+    OkHttpClient client = new OkHttpClient();
+    HttpUrl.Builder urlBuilder = HttpUrl.parse("https://www.googleapis.com/admin/directory/v1/customer/my_customer/devices/chromeos").newBuilder();
+    urlBuilder.addQueryParameter("maxResults", "3");
+    urlBuilder.addQueryParameter("projection", "FULL");
+    urlBuilder.addQueryParameter("sortOrder", "ASCENDING");
+    urlBuilder.addQueryParameter("key", "AIzaSyBq4godZxCMXHkkqLDSve1x27gCSYmBfVM");
+    String myUrl = urlBuilder.build().toString();
+    System.out.println(myUrl);
+    Request req = new Request.Builder()
+        .header("Authorization", accessToken)
+        .url(myUrl)
+        .build();
+    Response myResponse = client.newCall(req).execute();
+    System.out.println(myResponse.body().string());
+
+
+
   }
 
 }
