7f2c4def395748038fda6691e3dff4345b0f5a6d
George Zheng <georgezheng999@gmail.com>
2020-07-29 07:46:02 +0000

    move authorization logic into auth servlet

---
+++ src/main/java/com/google/sps/servlets/AuthorizeServlet.java
@@ -0,0 +1,96 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import com.google.sps.data.ChromeOSDevice;
+import com.google.sps.data.ListDeviceResponse;
+import com.google.sps.gson.Json;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest;
+import com.google.api.client.auth.oauth2.TokenResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import java.security.GeneralSecurityException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.HttpUrl;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.File;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@WebServlet("/authorize")
+public class DevicesServlet extends HttpServlet {
+
+  private final Gson GSON_OBJECT = new Gson();
+  private final String CLIENT_SECRET_FILE = "/client_info.json";
+  private final OkHttpClient client = new OkHttpClient();
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  }
+
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    final UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+        response.sendRedirect("/login");
+    }
+    final String authCode = (String) request.getParameter("code");
+    Entity tokenEntity = new Entity("RefreshToken");
+    final String userId = userService.getCurrentUser().getUserId();
+    final String refreshCode = getRefreshCode();
+    tokenEntity.setProperty("userId", userId);
+    tokenEntity.setProperty("refreshToken", refreshToken);
+    System.out.println("got the fresh token of: " + refreshToken);
+    datastore.put(tokenEntity);
+    response.sendRedirect("/index.html");
+  }
+
+  private String getRefreshCode(String authCode) {
+    File file = new File(this.getClass().getResource(CLIENT_SECRET_FILE).getFile());
+    final GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(
+            JacksonFactory.getDefaultInstance(), new FileReader(file));
+    final String clientId = clientSecrets.getDetails().getClientId();
+    final String clientSecret = clientSecrets.getDetails().getClientSecret();
+    final GoogleTokenResponse tokenResponse =
+            new GoogleAuthorizationCodeTokenRequest(
+                new NetHttpTransport(),
+                JacksonFactory.getDefaultInstance(),
+                "https://oauth2.googleapis.com/token",
+                clientSecrets.getDetails().getClientId(),
+                clientSecrets.getDetails().getClientSecret(),
+                authCode,
+                "http://localhost:8080") 
+                .execute();
+    final String refreshToken = tokenResponse.getRefreshToken();
+    return refreshToken;
+  }
+
+}
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,22 +1,37 @@
-function signInCallback(authResult) {
+function authorizeCallback(authResult) {
   if (authResult['code']) {
     $('#signinButton').attr('style', 'display: none');
     var xhr = new XMLHttpRequest();
     xhr.open("POST", '/devices', true);
-
-    //Send the proper header information along with the request
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    xhr.onreadystatechange = function() { // Call a function when the state changes.
+    xhr.onreadystatechange = function() { 
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-console.log("donne");
+            console.log("logged successfully");
         }
     }
     xhr.send("code=" + authResult['code']);
   } else {
     console.log("user is not signed in");
+    window.location.href = "/login";
   }
 }
+
+// function getDevices() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", '/devices', true);
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//     xhr.onreadystatechange = function() { // Call a function when the state changes.
+//         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+// console.log("donne");
+//         }
+//     }
+//     xhr.send("code=" + authResult['code']);
+//   } else {
+//     console.log("user is not signed in");
+//   }
+// }
 
 async function handleLogin() {
   fetch('/status').then(response => response.json()).then((isLoggedIn) => {
