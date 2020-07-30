0707c07b2d8712af2bf104ce174a1d1160ca8b67
George Zheng <georgezheng999@gmail.com>
2020-07-30 22:36:43 +0000

    removed unneeded imports from devices servlet

--- src/main/java/com/google/sps/servlets/DevicesServlet.java
+++ src/main/java/com/google/sps/servlets/DevicesServlet.java
@@ -2,64 +2,24 @@ package com.google.sps.servlets;
 
 import com.google.gson.Gson;
 import com.google.sps.data.ChromeOSDevice;
-import com.google.sps.data.ListDeviceResponse;
 import com.google.sps.servlets.Util;
 import com.google.sps.gson.Json;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest;
-import com.google.api.client.auth.oauth2.TokenResponseException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import java.security.GeneralSecurityException;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.HttpUrl;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.File;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query.FilterOperator;
 
 @WebServlet("/devices")
 public class DevicesServlet extends HttpServlet {
 
   private final Gson GSON_OBJECT = new Gson();
-  private final String CLIENT_SECRET_FILE = "/client_info.json";
-  private final OkHttpClient client = new OkHttpClient();
-  private final String INVALID_ACCESS_TOKEN = "INVALIDDD";
-  private final String EMPTY_REFRESH_TOKEN = "";
-  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -74,7 +34,6 @@ public class DevicesServlet extends HttpServlet {
     response.setContentType("application/json");
     final String json = GSON_OBJECT.toJson(allDevices);
     response.getWriter().println(json);
-    System.out.println("SUCCESS!!!\n\n\n\n\n");
   }
 
   @Override
