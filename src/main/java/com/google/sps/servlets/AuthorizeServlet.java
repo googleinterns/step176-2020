34b526796c58fed8da6ca72c981702c9734b5a8e
George Zheng <georgezheng999@gmail.com>
2020-07-30 22:40:08 +0000

    removed unneeded imports from author servlet

--- src/main/java/com/google/sps/servlets/AuthorizeServlet.java
+++ src/main/java/com/google/sps/servlets/AuthorizeServlet.java
@@ -1,8 +1,6 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
-import com.google.sps.data.ChromeOSDevice;
-import com.google.sps.data.ListDeviceResponse;
 import com.google.sps.gson.Json;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,19 +21,7 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.HttpUrl;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.File;
 import com.google.appengine.api.users.UserService;
@@ -45,13 +31,13 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.User;
 
 @WebServlet("/authorize")
 public class AuthorizeServlet extends HttpServlet {
 
   private final Gson GSON_OBJECT = new Gson();
   private final String CLIENT_SECRET_FILE = "/client_info.json";
-  private final OkHttpClient client = new OkHttpClient();
   private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
   @Override
@@ -63,18 +49,19 @@ public class AuthorizeServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     final UserService userService = UserServiceFactory.getUserService();
-    if (!userService.isUserLoggedIn()) {
+    final User currentUser = userService.getCurrentUser();
+    if ((!userService.isUserLoggedIn()) || (currentUser == null)) {
         response.sendRedirect("/login");
+        return;
     }
+    final String userId = currentUser.getUserId();
     final String authCode = (String) request.getParameter("code");
     Entity tokenEntity = new Entity("RefreshToken");
-    final String userId = userService.getCurrentUser().getUserId();
     final String refreshToken = getRefreshCode(authCode);
     tokenEntity.setProperty("userId", userId);
     tokenEntity.setProperty("refreshToken", refreshToken);
     datastore.put(tokenEntity);
     response.sendRedirect("/index.html");
-    System.out.println("refresh token stored as : " + refreshToken);
   }
 
   private String getRefreshCode(String authCode) throws IOException {
