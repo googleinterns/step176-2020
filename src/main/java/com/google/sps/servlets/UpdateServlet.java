ac94df1c91bc3421785e746d0681f73631766787
George Zheng <georgezheng999@gmail.com>
2020-08-06 13:46:32 +0000

    now uses a map to update

--- src/main/java/com/google/sps/servlets/UpdateServlet.java
+++ src/main/java/com/google/sps/servlets/UpdateServlet.java
@@ -4,6 +4,7 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.sps.data.ChromeOSDevice;
+import com.google.gson.Gson;
 import com.google.sps.gson.Json;
 import com.google.sps.servlets.Util;
 import com.squareup.okhttp.MediaType;
@@ -17,7 +18,9 @@ import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,9 +31,10 @@ import javax.servlet.http.HttpServletResponse;
 public class UpdateServlet extends HttpServlet {
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final Gson GSON_OBJECT = new Gson();
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     final UserService userService = UserServiceFactory.getUserService();
     final User currentUser = userService.getCurrentUser();
     if ((!userService.isUserLoggedIn()) || (currentUser == null)) {
@@ -50,21 +54,19 @@ public class UpdateServlet extends HttpServlet {
         Random rand = new Random();
         String newAnnotatedUser = users.get(rand.nextInt(7));
         String newAnnotatedLocation = locations.get(rand.nextInt(5));
-        updateDevice(accessToken, deviceId, newAnnotatedUser, newAnnotatedLocation);
+        Map<String, String> mp = new HashMap<>();
+        mp.put("annotatedUser", newAnnotatedUser);
+        mp.put("annotatedLocation", newAnnotatedLocation);
+        updateDevice(accessToken, deviceId, mp);
     }
           response.sendRedirect("/index.html");
-      return;
+    return;
   }
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-  }
-
-  private void updateDevice(String accessToken, String deviceId, String newAnnotatedUser, String newAnnotatedLocation) throws IOException {
+  private void updateDevice(String accessToken, String deviceId, Map<String, String> mp) throws IOException {
     final String myUrl = getUpdateUrl(deviceId);
     OkHttpClient client = new OkHttpClient();
-    String json = getJson(newAnnotatedUser, newAnnotatedLocation);
+    String json = getJsonFromMap(mp);
     RequestBody body = RequestBody.create(JSON, json);
     Request req = new Request.Builder().url(myUrl).put(body).addHeader("Authorization", "Bearer " + accessToken).build();
     Response myResponse = client.newCall(req).execute();
@@ -72,9 +74,9 @@ public class UpdateServlet extends HttpServlet {
     System.out.println("finished updating " + deviceId);
   }
 
-  private String getJson(String newAnnotatedUser, String newAnnotatedLocation) {
-                  return "{'annotatedUser':'" + newAnnotatedUser + "',"
-                + "'annotatedLocation':'" + newAnnotatedLocation + "',}";
+  private String getJsonFromMap(Map<String, String> mp) {
+        final String json = GSON_OBJECT.toJson(mp);
+        return json;
   }
 
   private String getUpdateUrl(String deviceId) {
