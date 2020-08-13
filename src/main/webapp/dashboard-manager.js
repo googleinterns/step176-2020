f76c6b9978048234ed4fbaed7bfb05f55508f82f
George Zheng <georgezheng999@gmail.com>
2020-08-13 16:27:45 +0000

    test removing script.js completely

--- src/main/webapp/dashboard-manager.js
+++ src/main/webapp/dashboard-manager.js
@@ -162,3 +162,23 @@ async function handleLogin() {
     }
   });
 }
+
+function authorizeCallback(authResult) {
+  if (authResult['code']) {
+    $('#signinButton').attr('style', 'display: none');
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/authorize', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() { 
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log("logged refresh token successfully");
+            window.location.href = "/index.html";
+        }
+    }
+    xhr.send("code=" + authResult['code']);
+  } else {
+    console.log("user is not authorized");
+    window.location.href = "/authorize.html";
+  }
+}
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,40 +1,40 @@
-function authorizeCallback(authResult) {
-  if (authResult['code']) {
-    $('#signinButton').attr('style', 'display: none');
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/authorize', true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// function authorizeCallback(authResult) {
+//   if (authResult['code']) {
+//     $('#signinButton').attr('style', 'display: none');
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", '/authorize', true);
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    xhr.onreadystatechange = function() { 
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            console.log("logged refresh token successfully");
-            window.location.href = "/index.html";
-        }
-    }
-    xhr.send("code=" + authResult['code']);
-  } else {
-    console.log("user is not authorized");
-    window.location.href = "/authorize.html";
-  }
-}
+//     xhr.onreadystatechange = function() { 
+//         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+//             console.log("logged refresh token successfully");
+//             window.location.href = "/index.html";
+//         }
+//     }
+//     xhr.send("code=" + authResult['code']);
+//   } else {
+//     console.log("user is not authorized");
+//     window.location.href = "/authorize.html";
+//   }
+// }
 
-function getDevices() {
-    const historyEl = document.getElementById('devices');
-    historyEl.innerHTML = 'things r loading!!!';
-    fetch("/devices").then(response => response.json())
-    .then(function(stuff) {
-        historyEl.innerHTML = JSON.stringify(stuff);
-    }).catch(function() {
-        console.log("error");
-        window.location.href = "/authorize.html";
-    });
-}
+// function getDevices() {
+//     const historyEl = document.getElementById('devices');
+//     historyEl.innerHTML = 'things r loading!!!';
+//     fetch("/devices").then(response => response.json())
+//     .then(function(stuff) {
+//         historyEl.innerHTML = JSON.stringify(stuff);
+//     }).catch(function() {
+//         console.log("error");
+//         window.location.href = "/authorize.html";
+//     });
+// }
 
 
-async function handleLogin() {
-  fetch('/status').then(response => response.json()).then((isLoggedIn) => {
-    if (!isLoggedIn) {
-      window.location.replace("/login");
-    }
-  });
-}
+// async function handleLogin() {
+//   fetch('/status').then(response => response.json()).then((isLoggedIn) => {
+//     if (!isLoggedIn) {
+//       window.location.replace("/login");
+//     }
+//   });
+// }
