4e487ba083b175c26570d699ee243dd5e7066096
Michael Valverde <mvalverde@google.com>
2020-08-25 13:04:31 -0400

    Remove old TODO.  The refactor either removed these functions or moved all of their complex logic elsewhere, which can better be tested in the tests for other files

--- src/test/webapp/dashboard-manager.test.js
+++ src/test/webapp/dashboard-manager.test.js
@@ -98,11 +98,6 @@ test('Fetching and Populating Aggregation Data', async () => {
   testAggregationAddRow(addData, 1, 'Jane', 2, "[\"1\",\"2\"]");
 });
 
-
-// TODO: add tests for initData, updateNormal, etc. after oauth gets merged in
-// because they will likely undergo significant changes
-
-
 // Corresponds to the data given for aggregation in global.fetch mock.
 function testAggregationAddRow(addRow, row, user, count, ids) {
   expect(addRow.mock.calls.length).toBe(2);
