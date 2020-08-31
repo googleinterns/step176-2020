5ff23572e671c9196e0fc227e503e7e7c31a5c3e
George Zheng <georgezheng999@gmail.com>
2020-08-31 15:31:08 +0000

    fixed location of auth flow

--- src/main/webapp/table.js
+++ src/main/webapp/table.js
@@ -1,9 +1,11 @@
 import {DashboardManager} from './dashboard-manager.js';
+import {handleLogin} from './auth-flow.js';
 
 google.charts.load('current', {'packages':['table', 'corechart', 'controls']});
 google.charts.setOnLoadCallback(dashboardInit);
 
 async function dashboardInit() {
+  await handleLogin();
   let dashboard = new DashboardManager();
   await dashboard.updateNormal();
   dashboard.draw();
