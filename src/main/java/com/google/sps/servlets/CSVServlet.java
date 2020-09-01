07c5e93d2382ae70a8b9feb71ab6b9822d6b1667
George Zheng <georgezheng999@gmail.com>
2020-09-01 15:08:49 +0000

    csv servlet now outputs correct devices list

--- src/main/java/com/google/sps/servlets/CSVServlet.java
+++ src/main/java/com/google/sps/servlets/CSVServlet.java
@@ -10,6 +10,7 @@ import com.google.sps.gson.Json;
 import com.google.sps.servlets.Util;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.StringBuilder;
 import java.security.GeneralSecurityException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +25,7 @@ public class CSVServlet extends HttpServlet {
   private UserService userService = UserServiceFactory.getUserService();
   private Util utilObj = new Util();
   public final String LOGIN_URL = "/login";
+  public final String HOME_URL = "/index.html";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -37,14 +39,25 @@ public class CSVServlet extends HttpServlet {
     response.setHeader("Content-Disposition", "attachment; filename=\"my_devices.csv\"");
     try {
       OutputStream outputStream = response.getOutputStream();
-      String outputResult = "xxxx, yyyy, zzzz, aaaa, bbbb, ccccc, dddd, eeee, ffff, gggg\n";
+      final List<ChromeOSDevice> allDevices = utilObj.getAllDevices(userId);
+      final String outputResult = stringifyDevicesList(allDevices);
       outputStream.write(outputResult.getBytes());
       outputStream.flush();
       outputStream.close();
+      response.sendRedirect(HOME_URL);
     } catch(Exception e) {
       System.out.println(e.toString());
       response.sendRedirect(LOGIN_URL);
     }
+  }
+
+  private String stringifyDevicesList(final List<ChromeOSDevice> allDevices) {
+    final String heading = "DEVICE ID,SERIAL NUMBER,ANNOTATED ASSET ID,ANNOTATED LOCATION,ANNOTATED USER\n";
+    final StringBuilder devicesStringBuilder = new StringBuilder(heading);
+    for (final ChromeOSDevice device : allDevices) {
+      devicesStringBuilder.append(device.getCSVLine());
+    }
+    return devicesStringBuilder.toString();
   }
 
   public void setUserService(UserService newUserService) {
